f5578e65f082894d882568f5a207bc5dd5110d5c
Marround <marround@yandex.ru>
2017-10-17 08:44:45 +0300

    update 16-10-2017 footer II

---
+++ src/app/appfooter/appfooter.component.ts
@@ -0,0 +1,11 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-footer',
+  templateUrl: './appfooter.component.html',
+  styleUrls: ['./appfooter.component.css']
+})
+export class AppfooterComponent {
+
+}
