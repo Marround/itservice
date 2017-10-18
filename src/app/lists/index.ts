a8c2817986686d295abcff5fb3e87cd5264c3420
Marround <marround@yandex.ru>
2017-10-18 22:49:08 +0300

    update 18-10-2017 footer

--- src/app/lists/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Marround on 10.10.2017.
- */
-export * from './list1/list1.component';
-export * from './list2/list2.component';
-export * from './lists.module';
-export * from './lists.routs';
-
